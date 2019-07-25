7597d8ac9af165bd233eee49cfbb03969928700c
Balthazar Gronon <bgronon@gmail.com>
2019-07-25 14:15:33 -0700

    Beautify

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 const puppeteer = require('puppeteer')
 const schedule = require('node-schedule')
 const sendgrid = require('@sendgrid/mail')
+const mjml2html = require('mjml')
 
 const config = require('./config')
 
 sendgrid.setApiKey(process.env.SMTP_PASS)
+
+const getHTML = ({ totalDue }) =>
+  mjml2html(`
+<mjml>
+  <mj-head>
+    <mj-attributes>
+      <mj-text font-family="Helvetica" />
+    </mj-attributes>
+  </mj-head>
+  <mj-body background-color="#fcfcfc">
+
+    <mj-section>
+      <mj-column background-color="white" padding-bottom="20px">
+
+        <mj-hero mode="fixed-height" height="330px" background-width="600px" background-height="330px" background-url="https://i.pinimg.com/originals/70/a9/cc/70a9cc38b030b0b0d36c8aa824d464d2.jpg" background-color="#2a3448" padding-bottom="30px">
+          <mj-text padding="20px" color="#ffffff" align="center" font-size="45px" line-height="45px" padding-top="130px" font-weight="900">
+            GOOD DAY!
+          </mj-text>
+        </mj-hero>
+
+        <mj-text font-size="15px">We found citations linked to your license plate that you might want to pay!</mj-text>
+
+        <mj-divider border-width="1px" border-color="#f5f5f5" />
+
+        <mj-text font-size="20px" padding-bottom="0px">Total Due</mj-text>
+        <mj-text font-size="20px" font-weight="bold">${totalDue}</mj-text>
+
+        <mj-button href="https://www.citationprocessingcenter.com/citizen-search-citation.aspx" align="left" background-color="blue">
+          PAY NOW
+        </mj-button>
+
+      </mj-column>
+    </mj-section>
+  </mj-body>
+</mjml>
+`).html
 
 const main = async ({ plate, email }) => {
   const browser = await puppeteer.launch()
@@ -33,7 +70,7 @@ const main = async ({ plate, email }) => {
         from: 'system@balthazar.dev',
         to: email,
         subject: `[Ticket Checker] Unpaid citations found on ${plate}`,
-        html: `<p>Total Due ${totalDue}</p>`,
+        html: getHTML({ totalDue }),
       })
 
       console.log(
@@ -53,3 +90,5 @@ const main = async ({ plate, email }) => {
 schedule.scheduleJob('0 * * * *', () => {
   config.forEach(main)
 })
+
+config.forEach(main)
